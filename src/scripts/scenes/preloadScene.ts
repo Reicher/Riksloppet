aeee37ad6930b929654fb5d9598563b1a02686a0
Reicher <Robin.o.Reicher@gmail.com>
2022-01-08 13:22:42 +0100

    Cleanup före bygge

--- src/scripts/scenes/preloadScene.ts
+++ src/scripts/scenes/preloadScene.ts
@@ -6,9 +6,6 @@ export default class PreloadScene extends Phaser.Scene {
   }
 
   preload() {
-    this.load.spritesheet('player', 'assets/img/Politiker.png', { frameWidth: 17, frameHeight: 32 });	
-    this.load.image('peng', 'assets/img/Peng.png')
-
     this.load.spritesheet('annie_run', 'assets/img/Annie_spring-Sheet.png', { frameWidth: 40, frameHeight: 80})
     this.load.image('gata', 'assets/img/Gata.png');
     this.load.image('himmel', 'assets/img/Himmel och skyline.png');
@@ -16,13 +13,6 @@ export default class PreloadScene extends Phaser.Scene {
     this.load.image('förgrund1', 'assets/img/Förgrund1.png');
     this.load.image('förgrund2', 'assets/img/Förgrund2.png');
     
-
-		// load images
-    this.load.image('mark', 'assets/img/mark.png');
-    this.load.image('bakgrund1', 'assets/img/bakgrund1.png');
-    this.load.image('bakgrund2', 'assets/img/bakgrund2.png');
-    this.load.image('bakgrund3', 'assets/img/bakgrund3.png');
-
     this.load.image('splash', 'assets/img/splash.png');
     this.load.image('title', 'assets/img/titel.png');
     this.load.image('levelSetting', 'assets/img/levelSettings.png');
@@ -35,7 +25,7 @@ export default class PreloadScene extends Phaser.Scene {
   create() {    
     console.log('PreLoad')
 
-    //this.scene.start('SplashScene')
-    this.scene.start('MainScene')
+    this.scene.start('SplashScene')
+    //this.scene.start('MainScene')
   }
 }
