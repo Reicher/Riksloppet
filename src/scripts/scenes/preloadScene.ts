bfb215ecc33cb3a5dfe4c256065ef46ef645091c
Reicher <Robin.o.Reicher@gmail.com>
2022-02-15 19:40:38 +0100

    Add new pics

--- src/scripts/scenes/preloadScene.ts
+++ src/scripts/scenes/preloadScene.ts
@@ -7,6 +7,7 @@ export default class PreloadScene extends Phaser.Scene {
     this.load.spritesheet('c_run', 'assets/img/Annie_spring-Sheet.png', { frameWidth: 40, frameHeight: 80 })
     this.load.spritesheet('c_stopp', 'assets/img/Annie_stopp-Sheet.png', { frameWidth: 40, frameHeight: 80 })
     this.load.spritesheet('c_slag', 'assets/img/Annie_slag-Sheet.png', { frameWidth: 40, frameHeight: 80 })
+    this.load.spritesheet('c_krock', 'assets/img/Annie_krock-Sheet.png', { frameWidth: 40, frameHeight: 80 })
     this.load.image('c_porträtt', 'assets/img/Annie_porträtt.png')
     this.load.image('kd_porträtt', 'assets/img/Ebba_porträtt.png')
     this.load.image('v_porträtt', 'assets/img/Nooshi_porträtt.png')
@@ -16,6 +17,11 @@ export default class PreloadScene extends Phaser.Scene {
     this.load.image('välj', 'assets/img/välj.png')
 
     this.load.spritesheet('statist', 'assets/img/statist.png', { frameWidth: 89, frameHeight: 100 })
+    this.load.spritesheet('åskådare_kille_kast', 'assets/img/åskådare_kille_kast-Sheet.png', {
+      frameWidth: 40,
+      frameHeight: 80
+    })
+    this.load.image('åskådare_kill', 'assets/img/åskådare_kille_bära.png')
     this.load.image('statist_kast', 'assets/img/statist_kast.png')
 
     this.load.image('gata', 'assets/img/Gata.png')
@@ -32,6 +38,7 @@ export default class PreloadScene extends Phaser.Scene {
     this.load.image('game_type_select', 'assets/img/GameTypeSelectScreen.png')
 
     this.load.image('bil', 'assets/img/Blå bil.png')
+    this.load.image('bil_röd', 'assets/img/parkerad_bil_röd.png')
 
     this.load.image('peng', 'assets/img/Peng.png')
     this.load.image('neddut', 'assets/img/neddut.png')
